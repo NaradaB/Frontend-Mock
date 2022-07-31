e8c1426efb923b9ce406f107362873329e6822b1
NaradaB <77276407+NaradaB@users.noreply.github.com>
2022-07-31 14:50:36 +0100

    Push backbone code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import React from "react";
+import { RecoilRoot, useRecoilState } from "recoil";
+import { facilities } from "../src/states/FacilitiesAtom";
+import NavBar from "./components/NavBar";
 function App() {
+  const [facilitiesList, setFacilitiesList] = useRecoilState(facilities);
+  const [facilityTags, setFacilityTags] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/facilities").then((response) => {
+      let responseArray = response.data.data;
+      console.log(responseArray);
+
+      responseArray.forEach((facility) => {
+        console.log(facility.tags);
+      });
+
+      responseArray.sort((facilityA, facilityB) =>
+        facilityA.name > facilityB.name ? 1 : -1
+      );
+
+      setFacilitiesList(responseArray);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      This is a test <NavBar></NavBar>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,5 @@
+function NavBar() {
+  return <div>This is the nav bar</div>;
+}
+
+export default NavBar;
